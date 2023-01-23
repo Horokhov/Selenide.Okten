2d1b8ff8381f9370dab32c7305c1f89aaede8b35
max_horokhov@tutanota.com <max_horokhov@tutanota.com>
2023-01-23 20:08:40 +0400

    1

--- src/test/java/test_package/FirstTest.java
+++ src/test/java/test_package/FirstTest.java
@@ -1,6 +1,7 @@
 package test_package;
 
 import base.BaseTest;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
@@ -14,17 +15,19 @@ import static com.codeborne.selenide.Selenide.$$;
 public class FirstTest extends BaseTest {
     @Test
     public void test() {
-        $(By.xpath("//input[@placeholder=\"Username\"]")).append("standard_user");
-        $(By.xpath("//input[@placeholder=\"Password\"]")).append("secret_sauce");
+        $(By.xpath("//input[@placeholder=\"Username\"]")).shouldBe(Condition.visible).append("standard_user");
+        $(By.xpath("//input[@placeholder=\"Password\"]")).shouldBe(Condition.visible).append("secret_sauce");
         $(By.xpath("//input[@id='login-button']")).click();
-        $(By.xpath("//select[@class='product_sort_container']")).click();
-        Selenide.sleep(1000);
-        Selenide.actions().sendKeys(Keys.ESCAPE).perform();
-        Selenide.sleep(3000);
-        String text = $(By.xpath("//div[@class=\"inventory_item_name\"][1]")).text();
-        for (String s : $$(By.xpath("//div[@class=\"inventory_item_name\"]")).texts()) {
-            System.out.println(s);
-        }
+//        $(By.xpath("//select[@class='product_sort_container']")).click();
+        Selenide.sleep(2000);
+//        Selenide.actions().sendKeys(Keys.ESCAPE).perform();
+//        Selenide.sleep(3000);
+//        String text = $(By.xpath("//div[@class=\"inventory_item_name\"][1]")).text();
+//        for (String s : $$(By.xpath("//div[@class=\"inventory_item_name\"]")).texts()) {
+//            System.out.println(s);
+//        }
+        $(By.xpath("//span[text()=\"Products\"]")).waitUntil(Condition.visible, 3000);
+
 
     }
 }
---
+++ src/test/java/test_package/WaitTest.java
@@ -0,0 +1,11 @@
+package test_package;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class WaitTest extends BaseTest {
+    @Test
+    public void testMethod() {
+
+    }
+}
